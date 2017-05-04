db58253f0a9c6992bef1eb289ce9f437598dbc17
manifest <riku.torenius@gmail.com>
2017-05-04 18:00:08 +0300

    bootstrap CSS added

--- testi.py
+++ testi.py
@@ -4,29 +4,35 @@ import re
 import os
 import webbrowser
 
+# get user input, load imports
 url = input("Anna artikkelin osoite: ")
 conn = urllib3.PoolManager()
 pageconn = conn.request('GET', url)
 pagedata = pageconn.data
 soup = BeautifulSoup(pagedata, 'html.parser')
 
+# id of the content div is ArticleBody and a random number sequence
 div_id = "ArticleBody"
 regexd = re.compile('%s-\d*'%div_id)
 
+# find the div
 article = soup.find("div", id=regexd)
 headline = soup.find('h1', attrs = {'class' : 'article-title'})
 author = soup.find('a', attrs = {'itemprop' : 'author'})
 
+
+# write the HTML-file
 file = open("hesaripage.html", "w+")
-file.write("<html><head><meta charset=\"UTF-8\"></head>")
+file.write("<html><head><meta charset=\"UTF-8\"><title>hs.fi reader</title><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\"></head>")
+file.write("<body><div class=\"container\">")
 file.write(headline.prettify())
 if author:
 	file.write(author.prettify())
 file.write(article.prettify())
-file.write("</html>")
+file.write("</div></html>")
 file.close()
 
+#open the file in browser
 webbrowser.open_new_tab('file://' + os.path.dirname(os.path.abspath(__file__)) + "/hesaripage.html")
-
 print("HTML-tiedosto valmis, avataan..")
 
